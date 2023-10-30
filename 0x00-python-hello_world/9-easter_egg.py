070735146303ae02e92ca520eea52366dfeac989
Mulongovictor <mulongovibu@gmail.com>
2023-10-30 16:28:02 -0700

    A Python script that prints “The Zen of Python”, by TimPeters, followed by a new line.

--- 0x00-python-hello_world/9-easter_egg.py
+++ 0x00-python-hello_world/9-easter_egg.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-str1 = """The Zen of Python, by Tim Peters
+print("""\
+The Zen of Python, by Tim Peters
 
 Beautiful is better than ugly.
 Explicit is better than implicit.
@@ -19,5 +20,4 @@ Now is better than never.
 Although never is often better than *right* now.
 If the implementation is hard to explain, it's a bad idea.
 If the implementation is easy to explain, it may be a good idea.
-Namespaces are one honking great idea -- let's do more of those!"""
-print(f"{str1}")
+Namespaces are one honking great idea -- let's do more of those!""")
