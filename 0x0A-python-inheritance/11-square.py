e3c19c5a799bdfce17f56531c96d10e62b5f76a4
Mulongovictor <mulongovibu@gmail.com>
2023-12-04 14:17:08 -0800

    Task 11

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Define a Square class that inherits from Rectangle (9-rectangle.py), (task based on 10-square.py)"""
+"""Define a Square class that inherits from Rectangle (9)"""
 
 
 Rectangle = __import__('9-rectangle').Rectangle
