3ab6f2012fe6bd8b0fd72a4ecf41630459a72628
Mulongovictor <mulongovibu@gmail.com>
2023-12-05 12:23:14 -0800

    Task 1

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -7,6 +7,5 @@ Contains function that writes to text file and returns num chars written
 
 def write_file(filename="", text=""):
     """writes to text file and returns num chars written"""
-
     with open(filename, mode="w", encoding="utf-8") as f:
         return(f.write(text))
