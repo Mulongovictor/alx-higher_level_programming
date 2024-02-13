add5cc04d368419c60967158809b23831ef33921
Mulongovictor <mulongovibu@gmail.com>
2024-02-12 22:03:39 -0800

    Task 3 corrected

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -6,5 +6,20 @@ class Rectangle {
       this.height = h;
     }
   }
+
+  /**
+   * @property {method} print - prints the rectangle using the character X
+   * @returns void
+   */
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      let s = '';
+      for (let j = 0; j < this.width; j++) {
+        s += 'X';
+      }
+      console.log(s);
+    }
+  }
 }
+
 module.exports = Rectangle;
